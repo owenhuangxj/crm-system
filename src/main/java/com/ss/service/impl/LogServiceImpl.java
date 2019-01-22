5427ef7ce27fa8fbfc8a531cfc027325d33f085d
lzn <961136007@qq.com>
2019-01-22 17:24:26 +0800

    日志部分

---
+++ src/main/java/com/ss/service/impl/LogServiceImpl.java
@@ -0,0 +1,46 @@
+package com.ss.service.impl;
+
+
+import com.ss.dao.LogMapper;
+import com.ss.entity.Log;
+import com.ss.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class LogServiceImpl implements LogService {
+    @Autowired
+    private LogMapper lm;
+
+    /*分页查询日志总数*/
+    @Override
+    public Integer selectLogNum() {
+        return lm.selectLogNum();
+    }
+
+    /*添加日志*/
+    @Override
+    public Integer getInsertLog(Log log) {
+        String date = new SimpleDateFormat("YYYY/MM/dd HH:mm:ss").format(new Date());
+        System.out.println("创建日志的时间:" + date);
+        return lm.getInsertLog(log);
+    }
+
+    /*查询日志所有内容*/
+    @Override
+    public List<Log> getSelectLog(Integer currentPage, Integer currentPageSize) {
+        return lm.getSelectLog((currentPage-1)*currentPageSize,currentPageSize);
+    }
+
+    /*按要求查找日志内容*/
+    @Override
+    public List<Log> getAsyncSelectLog(String logType,String email,String userName,String startTime, String endTime,Integer currentPage, Integer currentPageSize) {
+        return lm.getAsyncSelectLog(logType,email,userName,startTime,endTime,(currentPage-1)*currentPageSize,currentPageSize);
+    }
+
+
+}
