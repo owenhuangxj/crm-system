07cd3e1bef4ea5fd2ca1c6e8433452f6a957dfb5
SummersKing <moshenwuji@live.com>
2019-01-23 10:34:33 +0800

    测试提交

---
+++ browser/src/views/menu/student/resume-allot.vue
@@ -0,0 +1,264 @@
+<template>
+
+    <div id="resumeAllot">
+
+
+
+        <div id="dialog-main">
+            <div id="msgTab" ref="msgTab">
+                <a @click="tabSwitch(index)"
+                   v-for="(item,index) in tabs"
+                   :class="{active : index===curId}"
+                >{{item.name}}</a>
+            </div>
+            <!--横线--> <!--滑动条-->
+            <div id="tabScroll" class="split">
+                <div ref="smallScroll" class="smallScroll"></div>
+            </div>
+            <!--人员选择-->
+            <div id="tab-emp">
+                <span>人员：</span>
+                <el-select v-model="employeeSelect" multiple placeholder="请选择">
+                    <el-option
+                            v-for="item in employees"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                    </el-option>
+                </el-select>
+
+                <div id="allot-time">
+                    <span>分配数量</span>
+                    <el-input-number v-model="num" :step="3" :min="1" :max="100" label="描述文字"></el-input-number>
+                </div>
+                <div>
+                    <div id="allot_time">
+                        <span>分配时间：</span>
+                        <el-radio v-model="radio" label="1" @change="hideTimePicker()">立刻发送</el-radio>
+                        <el-radio v-model="radio" label="2" @change="showTimePicker()">选择时间</el-radio>
+                        <br/>
+                        <div id="timePicker">
+                            <el-date-picker size=="mini" v-model="dateDefault" type="datetime" placeholder="选择任务时间" align="right" :picker-options="timePicker"></el-date-picker>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+
+
+    </div>
+
+
+</template>
+
+<script>
+ import {getAllUsers} from '@/api/allot'
+
+  export default {
+        data: function () {
+            return {
+                /*时间选框*/
+                timePicker: {
+                    shortcuts: [{
+                        text: '今天下午',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setHours(14);
+                            date.setMinutes(0);
+                            date.setSeconds(0);
+                            if (date > new Date()) {
+                                picker.$emit('pick', date);
+                            } else {
+                                picker.$emit('pick', new Date());
+                            }
+
+                        }
+                    }, {
+                        text: '明天早上',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setHours(9);
+                            date.setDate(date.getDate() + 1);
+                            date.setMinutes(0);
+                            date.setSeconds(0);
+                            date.setTime(date.getTime());
+                            picker.$emit('pick', date);
+                        }
+                    }]
+                },
+                radio: 'allotTime',
+                dateDefault: '',
+                curId: 0,
+                // 标签名
+                title: "简历分配",
+                //数字框每次增量
+                val: 5,
+                //初始量
+                num: 5,
+                tabs: [
+                    {name: '固定分配'},
+                    {name: '随机分配'},
+
+                ],
+                /*下拉框中员工*/
+                employees: [],
+                employeeSelect: [],
+            }
+        },
+        created() {
+            console.log("creates方法执行")
+        },
+        computed: {
+            data() {
+                alert("computed方法执行")
+            },
+            tableData: function () {
+                var _search = this.select_word;
+                if (_search) {
+                    return this.tableData.filter(function (product) {
+                        return Object.keys(product).some(function (key) {
+                            return String(product[key]).toLowerCase().indexOf(_search) > -1
+                        })
+                    })
+                }
+                return this.tableData;
+            }
+        },
+        mounted: function () {
+            console.log("mounted执行");
+           getAllUsers().then(response => {
+              this.employees = response.data;
+              console.log(response.data);
+            })
+        },
+        methods: {
+            //选项卡变化
+            tabSwitch(index) {
+                this.curId = index;
+                console.log(index)
+                setTimeout(tabScrollMove, 10, this);
+                console.log('执行Tab');
+            },
+            showTimePicker() {
+                document.getElementById("timePicker").style.display = "block";
+            },
+            hideTimePicker() {
+                document.getElementById("timePicker").style.display = "none";
+            },
+            fixAllot() {
+
+            },
+            randomAllot() {
+
+            },
+            handleClose(done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                        done();
+                    })
+                    .catch(_ => {
+                    });
+            },
+        },
+        /*监控函数*/
+        watch: {
+            dateDefault: {
+                deep: true,
+                handler: function (newVal, oldVal) {
+                    console.log('new:', newVal, 'old:', oldVal);
+                },
+            },
+                curId: {
+                    deep: true,
+                    handler: function (newVal, oldVal) {
+
+                        if(0==newVal){
+
+                            document.getElementById("allotNum").style.display = "none";
+                        }else{
+                            document.getElementById("allotNum").style.display = "block";
+                        }
+
+                        console.log('new:', newVal, 'old:', oldVal);
+                    }
+                }
+            }
+
+
+    }
+
+    function tabScrollMove(obj) {
+        obj.$refs.smallScroll.style.left = document.getElementsByClassName("active")[0].offsetLeft + 'px';
+    }
+
+
+</script>
+
+<style scoped>
+
+    #dialog-main {
+        padding-left: 2em;
+        padding-right: 2em;
+    }
+
+    #resumeAllot {
+        padding: 30px;
+        background: #fff;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        height: 500px;
+    }
+
+    #msgTab {
+        height: 50px;
+
+    }
+    #allotNum{
+        display: none;
+    }
+
+    #timePicker {
+        display: none;
+    }
+
+    a {
+        color: #000;
+        text-decoration: none;
+    }
+
+    #msgTab > a {
+        float: left;
+        padding-right: 40px;
+        height: 70px;
+        line-height: 70px;
+        font-size: 14px;
+    }
+
+    #msgTab > a:hover {
+        color: #409eff;
+    }
+
+    /*分割线*/
+    .split {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        background-color: #f0f0f0;
+        height: 0.2rem;
+    }
+
+    .smallScroll {
+        /*左右位置过渡*/
+        transition: left .4s;
+        position: absolute;
+        width: 4rem;
+        height: 0.2rem;
+        background-color: #409eff;
+    }
+
+
+</style>
---
+++ browser/src/views/menu/student/resume-import.vue
@@ -0,0 +1,55 @@
+<template>
+
+<div>
+           <span>选择模板：</span> <el-select v-model="value" placeholder="请选择">
+
+         <el-option
+                    v-for="item in fileModel"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+            </el-option>
+
+        </el-select>
+
+        <el-upload
+                class="upload-demo"
+                ref="upload"
+                :limit=1
+                action="http://localhost/importResumeFromExcel"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :auto-upload="false">
+            <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+            <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传数据</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传xls、xlsx文件</div>
+        </el-upload>
+    </div>
+
+</template>
+<script>
+    let fileModel=[{label:'58同城',value:1},{label:'boss直聘',value:2},{label:'赶集网',value:3}]
+    export default{
+        data() {
+            return {
+                fileModel:fileModel,
+                value: '',
+                fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+            };
+        },
+        methods: {
+            submitUpload() {
+                this.$refs.upload.submit();
+            },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
